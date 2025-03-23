f4bc12444ebeace0ede2d81d24fb08962b263338
chael412 <165856017+chael412@users.noreply.github.com>
2025-03-23 22:15:56 +0800

    first commit

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,10 @@
+import { Head, Link } from "@inertiajs/react";
+import { Button } from "@/components/ui/button";
+
+export default function Welcome({ auth, laravelVersion, phpVersion }) {
+    return (
+        <>
+            <Button variant="outline">Button</Button>
+        </>
+    );
+}
